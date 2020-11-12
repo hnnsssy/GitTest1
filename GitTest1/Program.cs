8fdc07edb4648d65a21a2ea1db229d7138330940
lilyakaras <lilyca.hrupa@gmail.com>
2020-11-12 10:58:51 +0200

    branch test 1

--- GitTest1/Program.cs
+++ GitTest1/Program.cs
@@ -11,7 +11,7 @@ namespace GitTest1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello word!");
-            Console.WriteLine("Hello word!");
+            Console.WriteLine("Hello word from br1!");
         }
     }
 }
